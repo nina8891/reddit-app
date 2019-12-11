ddc7baf1781c51a269af60ffdfbbc9909b4345f0
Nina <nina.gs20@yandex.ru>
2019-12-11 16:00:20 +0400

    Updates

---
+++ precache-manifest.f7a6b9c2379d57510e4136caf11a7d60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2993a9c19d6fd5a7ebd543598c1cc8c4",
+    "url": "/reddit-app/index.html"
+  },
+  {
+    "revision": "e46f51924006b95fdb08",
+    "url": "/reddit-app/static/css/main.fc26d359.chunk.css"
+  },
+  {
+    "revision": "d98be9132129bd5c817d",
+    "url": "/reddit-app/static/js/2.41489324.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/reddit-app/static/js/2.41489324.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e46f51924006b95fdb08",
+    "url": "/reddit-app/static/js/main.0b2f2e36.chunk.js"
+  },
+  {
+    "revision": "276b2c2c53531c1255af",
+    "url": "/reddit-app/static/js/runtime-main.6a1bd906.js"
+  }
+]);
